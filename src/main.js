91410a89f56ab5c375701e85d4000892ddfb7c09
TheWebDog <2725550609@qq.com>
2022-08-05 16:09:19 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import 'mavon-editor/dist/css/index.css'
 import axios from "axios"
 
 // 全局配置axios的请求根路径
-// axios.defaults.baseURL = "http://localhost:4000"
+// axios.defaults.baseURL = "http://localhost:6060"
 axios.defaults.baseURL = "https://blog-backend-kappa.vercel.app"
 
 // 将axios挂载到Vue上
