d482b8ec76508321c021a4d6a80e2862fb1fe076
TheWebDog <2725550609@qq.com>
2022-08-08 02:15:01 +0800

    检查bug

--- src/components/list/views/List.vue
+++ src/components/list/views/List.vue
@@ -3,20 +3,12 @@
     <el-row :gutter="12">
       <el-col :span="24">
         <ul class="list_ul">
-          <li
-            class="list_li"
-            v-for="(item, index) in get_articleList"
-            :key="index"
-            @click="to_article(item._id)"
-            v-scroll-reveal.reset
-          >
+          <li class="list_li" v-for="(item, index) in get_articleList" :key="index" @click="to_article(item._id)"
+            v-scroll-reveal.reset>
             <el-card class="list_li_div" shadow="hover">
-              <el-image
-                class="el_card_el_image"
-                :src="getPicbase64(item.coverRequirePath)"
-                fit="contain"
-              ></el-image>
+              <el-image class="el_card_el_image" :src="getPicbase64(item.coverRequirePath)" fit="contain"></el-image>
               <!-- {{item.coverRequirePath}} -->
+              {{getPicbase64(item.coverRequirePath)}}
               <ul class="textIntroduction_ul">
                 <li class="textIntroduction_li">
                   <div class="el_card_title">{{ item.title }}</div>
@@ -42,15 +34,8 @@
     </el-row>
 
     <!-- 分页 -->
-    <el-pagination
-      :small="media_lessThan_width500px"
-      background
-      layout="prev, pager, next"
-      :page-size="10"
-      :pager-count="5"
-      :total="get_articleList.length+20"
-      @current-change="handleCurrentChange"
-    >
+    <el-pagination :small="media_lessThan_width500px" background layout="prev, pager, next" :page-size="10"
+      :pager-count="5" :total="get_articleList.length + 20" @current-change="handleCurrentChange">
     </el-pagination>
   </div>
 </template>
@@ -62,11 +47,11 @@ export default {
   name: "List",
   data () {
     return {
-      screenWidth:  document.body.scrollWidth,
+      screenWidth: document.body.scrollWidth,
     }
   },
   computed: {
-    ...mapGetters(['get_articleList','get_picUrl']),
+    ...mapGetters(['get_articleList', 'get_picUrl']),
     media_lessThan_width500px () {
       return this.screenWidth < 500
     },
@@ -81,18 +66,18 @@ export default {
     },
     getPicbase64 (coverRequirePath) {
       var picUrl
-        axios
-          .get(coverRequirePath)
-          .then((res) => {
-            picUrl = res.data
-            return picUrl
-          })
-          .catch((err) => {
-           console.log(err)
-          })
+      axios
+        .get(coverRequirePath)
+        .then((res) => {
+          picUrl = res.data
+          return picUrl
+        })
+        .catch((err) => {
+          console.log(err)
+        })
     }
 
-    
+
   },
   created () {
     this.action_getArticleList(this.$router.currentRoute.fullPath)
